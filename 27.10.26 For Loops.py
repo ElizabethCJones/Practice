cadc9fd2e40bbb90bbe1f1e7b6c341930dc1c943
Liz <elizabethcjones@outlook.com>
2025-10-27 19:20:21 +0000

    Reformat comments and code in multiplication table program

--- 27.10.26 For Loops.py
+++ 27.10.26 For Loops.py
@@ -2,12 +2,13 @@
 # The program should display the multiplication tables for numbers 1 through 5. 
 # Each table should list the products of the number with every number from 1 to 5. 
 # After completing the table for one number, the program should clearly separate it from the next number’s table.
-#Write a Python program using nested for loops to achieve this. Expected output
+#Write a Python program using nested for loops to achieve this. 
 
 for number in range(1, 6):  
     print(f"{number} Times Tables: ")
     for i in range(1, 6): 
         print(f"{number} x {i} = {number * i}")
     print("-" * 10)
+
 
 
